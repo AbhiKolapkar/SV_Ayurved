40f49164f17554b4a16fb7c698ae53a15325af53
AbhiKolapkar <abhishek@skillnuts.in>
2023-09-03 11:47:43 +0530

    header updated with sticky animation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { Container, useMediaQuery } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/icons/SV_Logo.svg";
@@ -10,9 +10,23 @@ const Header = () => {
   const isDesktop = useMediaQuery("(min-width:" + 1240 + "px)");
   const isMobile = useMediaQuery("(max-width:" + 900 + "px)");
 
+  const stickyHeader = useRef()
+  useLayoutEffect(() => {
+    const header = document.querySelector('header')
+    const fixedHeader = () => {
+      if(window.pageYOffset > 200) {
+        header.classList.add(`${styles.is_sticky}`)
+      }
+      else {
+        header.classList.remove(`${styles.is_sticky}`)
+      }
+    }
+    window.addEventListener('scroll', fixedHeader)
+  }, [])
+
   return (
     <>
-      <header className={styles.header}>
+      <header className={styles.header} ref={stickyHeader}>
         <Container maxWidth="xxl">
           <div className={styles.toolbar}>
             <div className="imgBox">
